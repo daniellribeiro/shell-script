fee634b5eda0a79a1d6398e9b31a97ef955a7a66
Daniel <daniellopes03@hotmail.com>
2018-11-22 18:36:18 -0200

    Realizando algumas alteracoes

--- aula_so2/organizar_musicas.sh
+++ aula_so2/organizar_musicas.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#cores
+padrao="\033[0m"
+verde="\033[0;32m"
+OK=$(echo -e [$verde OK $padrao] )
+
+#variaveis
 ARQUIVO=arquivo_temporario
 ARQUIVO_ARTISTA=arquivo_artista
 ARQUIVO_ALBUM=arquivo_album
@@ -12,19 +18,18 @@ fi
 echo $(ls $PASTA | rev | cut -d- -f 2 | rev) > $ARQUIVO
 
 #Criando pasta dos artistas
-echo Criando pasta dos artistas [ OK ]
+echo Criando pasta dos artistas e movendo para sua respectiva pasta $OK
 for i in $(seq $(ls $PASTA | wc -l));do
 	artista=$(echo $(cut -d' ' -f $i $ARQUIVO));
 	if [ ! -d  $PASTA_TEMP/$artista ];then
 		mkdir $PASTA_TEMP/$artista
-		echo criou pasta
+		 mv $PASTA/*$artista* $PASTA_TEMP/$artista
 	fi
 	#Movendo musicas para pasta do respectivo artista
-	mv $PASTA/*$artista* $PASTA_TEMP/$artista
 done
 
 #Criando pasta dos albuns
-echo Criando pasta dos albuns [ OK ]
+echo Criando pasta dos albuns e movendo para sua respectiva pasta $OK
 echo $(ls $PASTA_TEMP) > $ARQUIVO_ARTISTA
 i=0;
 for i in $(seq $(ls $PASTA_TEMP | wc -l));do
@@ -35,6 +40,16 @@ for i in $(seq $(ls $PASTA_TEMP | wc -l));do
 		if [ ! -d $PASTA_TEMP/$PASTA_ARTISTA/$album ];then
 			mkdir $PASTA_TEMP/$PASTA_ARTISTA/$album
 		fi
-		mv $PASTA_TEMP/$PASTA_ARTISTA/*$album* $PASTA_TEMP/$PASTA_ARTISTA/$album
+		#movendo musicas para pasta do respectivo album
+		mv $PASTA_TEMP/$PASTA_ARTISTA/*$album* $PASTA_TEMP/$PASTA_ARTISTA/$album 2>/dev/null
 	done
 done
+
+#Movendo para pasta original
+echo Finalizando $OK
+mv $PASTA_TEMP/* $PASTA
+
+#Apagando arquivos temporarios
+echo Apagando arquivos temporarios $OK
+rmdir $PASTA_TEMP
+rm $ARQUIVO $ARQUIVO_ARTISTA $ARQUIVO_ALBUM
