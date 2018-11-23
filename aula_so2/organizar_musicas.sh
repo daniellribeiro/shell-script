3e7865f2ef7569bae4271dc78e505a07256487e4
Daniel <daniellopes03@hotmail.com>
2018-11-23 16:51:29 -0200

    Mudei o nome de algumas variaveis

--- aula_so2/organizar_musicas.sh
+++ aula_so2/organizar_musicas.sh
@@ -2,14 +2,14 @@
 #cores
 padrao="\033[0m"
 verde="\033[0;32m"
-OK=$(echo -e [$verde OK $padrao] )
+SUCESSO=$(echo -e [$verde SUCESSO $padrao] )
 
 #variaveis
-ARQUIVO=arquivo_temporario
+ARQUIVO=arquivo
 ARQUIVO_ARTISTA=arquivo_artista
 ARQUIVO_ALBUM=arquivo_album
 PASTA=musicas
-PASTA_TEMP=organizados
+PASTA_TEMP=temporaria
 
 if [ ! -d "$PASTA_TEMP" ];then
 	mkdir $PASTA_TEMP;
@@ -18,18 +18,17 @@ fi
 echo $(ls $PASTA | rev | cut -d- -f 2 | rev) > $ARQUIVO
 
 #Criando pasta dos artistas
-echo Criando pasta dos artistas e movendo para sua respectiva pasta $OK
+echo Movendo musicas para pasta do artista correto $SUCESSO
 for i in $(seq $(ls $PASTA | wc -l));do
 	artista=$(echo $(cut -d' ' -f $i $ARQUIVO));
 	if [ ! -d  $PASTA_TEMP/$artista ];then
 		mkdir $PASTA_TEMP/$artista
-		 #Movendo musicas para pasta do respectivo artista
 		 mv $PASTA/*$artista* $PASTA_TEMP/$artista 2>/dev/null
 	fi
 done
 
 #Criando pasta dos albuns
-echo Criando pasta dos albuns e movendo para sua respectiva pasta $OK
+echo Movendo musicas para pasta dos albuns corretos $SUCESSO
 echo $(ls $PASTA_TEMP) > $ARQUIVO_ARTISTA
 i=0;
 for i in $(seq $(ls $PASTA_TEMP | wc -l));do
@@ -40,16 +39,13 @@ for i in $(seq $(ls $PASTA_TEMP | wc -l));do
 		if [ ! -d $PASTA_TEMP/$PASTA_ARTISTA/$album ];then
 			mkdir $PASTA_TEMP/$PASTA_ARTISTA/$album
 		fi
-		#movendo musicas para pasta do respectivo album
 		mv $PASTA_TEMP/$PASTA_ARTISTA/*$album* $PASTA_TEMP/$PASTA_ARTISTA/$album/ 2>/dev/null
 	done
 done
 
-#Movendo para pasta original
-echo Finalizando $OK
+echo Finalizando $SUCESSO
 mv $PASTA_TEMP/* $PASTA
 
-#Apagando arquivos temporarios
-echo Apagando arquivos temporarios $OK
+echo Limpando arquivos temporarios $SUCESSO
 rmdir $PASTA_TEMP
 rm $ARQUIVO $ARQUIVO_ARTISTA $ARQUIVO_ALBUM
